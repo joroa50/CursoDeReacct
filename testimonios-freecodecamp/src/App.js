888e3963c86be4d4dd8e76814ba2e1e93c134a91
JOSE RODRIGO ROA ACOSTA <163214980+joroa50@users.noreply.github.com>
2024-11-12 13:33:12 +0000

    Aqui encontramos la app de calculadora, contador de clics-mi app y testimonios

---
+++ testimonios-freecodecamp/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import Testimonio from './componentes/Testimonio';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor principal'>
+      <h1>Esto es lo que nuestros exalumnos dicen sobre freeCodeCamp:</h1>
+     
+      <Testimonio
+      nombre='Shawn Wang'
+      pais=' Singapore'
+      imagen='shawn'
+      cargo='Ingeniero de software'
+      empresa='Amazon'
+      testimonio='Da miedo cambiar de carrera. Solo adquirí la confianza de que podía programar tras trabajar cientos de horas de lecciones gratuitas en freeCodeCamp. En menos de un año conseguí un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'/>
+       <Testimonio
+      nombre='Sarah Chima '
+      pais=' Nigeria'
+      imagen='Sarah'
+      cargo='Ingeniera de software'
+      empresa='ChatDesk'
+      testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel de principiante total a un nivel de mucha confianza. Era todo lo que necesitaba para conseguir mi primer trabajo como desarrollador en una empresa increíble.'/>
+       <Testimonio
+      nombre='Emma Bostian'
+      pais=' Suecia'
+      imagen='emma'
+      cargo='Ingeniera de software'
+      empresa='Spotify'
+      testimonio='Siempre me costó aprender JavaScript. Hice muchos cursos, pero el de freeCodeCamp fue el que me quedó grabado. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp, me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
